4a0660f7c51f73109b6c943d40f1b392c0c64e29
Peter Levinsky <pele@easj.dk>
2021-09-17 10:53:26 +0200

    Add project files.

---
+++ ClassDemo2021Fuktioner/InsertCodeHere.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassDemo2021Fuktioner
+{
+    public class InsertCodeHere
+    {
+        public InsertCodeHere()
+        {
+        }
+
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            double b = 34;
+            double l = 12;
+
+            double areal = ArealAfRektangel(b, l);
+            Console.WriteLine($"bredde {b} længde {l} giver arealet {areal}");
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+
+        // en metode eller funktion
+
+        public double ArealAfRektangel(double bredde, double længde)
+        {
+            // lave koden
+
+            double areal = bredde * længde;
+            return areal;
+            // return bredde * længde;
+
+        }
+    }
+}
